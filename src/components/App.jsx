fef05d51faa7d0ad73f39fd4ef0979bd0527cc64
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-02-18 23:26:41 +0200

    maybe ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Form } from 'components/Form/Form';
+import { ContactList } from './ContactList/ContactList';
+import { MainWrapper } from './MainWrapper.styled';
+import { Filter } from './Filter/Filter';
+import { Toaster } from 'react-hot-toast';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <MainWrapper>
+      <Toaster position="top-right" reverseOrder={false} />
+
+      <h1>Phonebook</h1>
+      <Form />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </MainWrapper>
   );
 };
